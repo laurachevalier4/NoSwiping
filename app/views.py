b54a9c793cfc0d41e4e4cb10efb09f8b41eea1d6
Vincent Chov <vincentchov@gmail.com>
2016-10-22 09:00:08 -0400

    Add run.py and copy the contents of routes.py to views.py

--- app/views.py
+++ app/views.py
@@ -1,1 +1,18 @@
-print("Hello")
+from app import app
+
+# This is the homepage/categories page!
+@app.route('/')
+@app.route('/index.html')
+@app.route('categories')
+def categories():
+    return "There will be categories here. Eventually. Tomorrow for sure..."
+
+# Once you choose a category, show some transactions from that category
+@app.route('/categories/<category_name>')
+def category(category_name):
+    return "You chose the %s category!" %(category_name)
+
+# The page for a specific transaction
+@app.route('/transactions/<transaction_id>')
+def transaction(transaction_id):
+    return "You chose the %s transaction!" %(transaction_id)
